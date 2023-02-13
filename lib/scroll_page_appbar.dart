8250272f0c46606904b4bba2d0b86ac85c510db6
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-02-13 17:57:08 +0800

    listview

--- lib/scroll_page_appbar.dart
+++ lib/scroll_page_appbar.dart
@@ -10,23 +10,30 @@ class ScrollPageAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight + 140),
-        child: Hero(
-          tag: 'appbar $color',
-          child: AppBar(
-              backgroundColor: color,
-              bottom: const PreferredSize(
-                preferredSize: Size.fromHeight(0),
-                child: Expanded(
-                  child: Text(
-                    'This is a title blblaa',
-                    style: TextStyle(fontSize: 34),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight + 140),
+          child: Hero(
+            tag: 'appbar $color',
+            child: AppBar(
+                backgroundColor: color,
+                bottom: const PreferredSize(
+                  preferredSize: Size.fromHeight(0),
+                  child: Expanded(
+                    child: Text(
+                      'This is a title blblaa',
+                      style: TextStyle(fontSize: 34),
+                    ),
                   ),
-                ),
-              )),
+                )),
+          ),
         ),
-      ),
-    );
+        body: ListView(
+          children: List.generate(
+            100,
+            (index) => ListTile(
+              title: Text('Item $index'),
+            ),
+          ),
+        ));
   }
 }
